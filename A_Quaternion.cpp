a71986481873e17997e36ea578f9f88c5539a820
Anthony Cusimano <anthonycusimano91@hotmail.com>
2017-07-30 23:27:01 -0400

    Add files via upload
    Todo: -optimize arguments by using pointers / references
    -add 2D vector class
    - at matrix support

---
+++ A_Quaternion.cpp
@@ -0,0 +1,201 @@
+#include "A_Quaternion.h"
+
+
+A_Quaternion::A_Quaternion() {
+
+	this->demensions[0] = 0.0f;
+	this->demensions[1] = 0.0f;
+	this->demensions[2] = 0.0f;
+	this->demensions[3] = 0.0f;
+
+}
+
+A_Quaternion::A_Quaternion(A_Quaternion const *_AQ) {
+
+	this->demensions[0] = _AQ->demensions[0];
+	this->demensions[1] = _AQ->demensions[1];
+	this->demensions[2] = _AQ->demensions[2];
+	this->demensions[3] = _AQ->demensions[3];
+
+}
+
+A_Quaternion::A_Quaternion(float const _x, float const _y, float const _z, float const _w) {
+
+	this->demensions[0] = _x;
+	this->demensions[1] = _y;
+	this->demensions[2] = _z;
+	this->demensions[3] = _w;
+	
+}
+
+A_Quaternion::A_Quaternion(A_Vector3 const *_AV, float const _w) {
+
+	this->demensions[0] = _AV->getX();
+	this->demensions[1] = _AV->getY();
+	this->demensions[2] = _AV->getZ();
+	this->demensions[3] = _w;
+
+}
+
+const A_Quaternion A_Quaternion::GetQuaternion(float _angle, A_Vector3 _axis) {
+
+	return A_Quaternion(_axis.getX(), _axis.getY(), _axis.getZ(), _angle);
+
+}
+
+const A_Quaternion A_Quaternion::GetQuaternion(float _x, float _y, float _z, float _w) {
+
+	return A_Quaternion(_x, _y, _z, _w);
+
+}
+
+const float A_Quaternion::GetMagnatude() {
+
+	float T_Mag = this->demensions[0] * this->demensions[0];
+	T_Mag += (this->demensions[1] * this->demensions[1]);
+	T_Mag += (this->demensions[2] * this->demensions[2]);
+	T_Mag += (this->demensions[3] * this->demensions[3]);
+	T_Mag = ::sqrtf(T_Mag);
+
+	return T_Mag;
+
+}
+
+void A_Quaternion::Normalize() {
+
+	/******************************************
+	TODO: ADD THRESHOLD
+	******************************************/
+
+	float T_Mag = this->GetMagnatude();
+
+	this->demensions[0] /= T_Mag;
+	this->demensions[1] /= T_Mag;
+	this->demensions[2] /= T_Mag;
+	this->demensions[3] /= T_Mag;
+
+}
+
+const float A_Quaternion::GetNormal() {
+
+	float T_Mag = this->GetMagnatude();
+
+	this->demensions[0] /= T_Mag;
+	this->demensions[1] /= T_Mag;
+	this->demensions[2] /= T_Mag;
+	this->demensions[3] /= T_Mag;
+
+	return T_Mag;
+
+}
+
+void A_Quaternion::Conjugate() {
+
+	this->demensions[0] *= -1.0f;
+	this->demensions[1] *= -1.0f;
+	this->demensions[2] *= -1.0f;
+
+}
+
+const A_Quaternion A_Quaternion::GetConjugate() {
+
+	A_Quaternion T_Conj;
+
+	T_Conj.demensions[0] = this->demensions[0] * -1.0f;
+	T_Conj.demensions[1] = this->demensions[1] * -1.0f;
+	T_Conj.demensions[2] = this->demensions[2] * -1.0f;
+	T_Conj.demensions[3] = this->demensions[3];
+
+	return T_Conj;
+
+}
+
+void A_Quaternion::SetFromEuler(A_Vector3 const &_Euler) {
+
+	float T_cosRoll, T_cosPitch, T_cosYaw, T_sinRoll, T_cosPitch2, T_cosYaw2;
+
+	//I would like to formally apologize to anyone reading this code in the future for the breakdown in naming conventions here
+	float T_CosPitchxT_CosYaw, T_CosPitch2xT_CosYaw, T_CosPitchxT_CosYaw2, T_cosPitch2xT_CosYaw;
+
+	float T_roll, T_pitch, T_yaw;
+
+	T_roll = _Euler.getX() * 0.5f;
+	T_pitch = _Euler.getY() * 0.5f;
+	T_yaw = _Euler.getZ() * 0.5f;
+
+	T_cosRoll = ::cosf(T_roll);
+	T_cosPitch = ::cosf(T_pitch);
+	T_cosYaw = ::cosf(T_yaw);
+
+	T_sinRoll = ::sinf(T_roll);
+	T_cosPitch2 = ::cosf(T_pitch);
+	T_cosYaw2 = ::cosf(T_yaw);
+
+	//these variables done this way to avoid making the same multiplication multiple times down below
+	//consider trying a different way to ease up on memory slightly while slightly slowing down arithmetic
+	//move variable declaration above
+
+	T_CosPitchxT_CosYaw/*T_CYxCZ*/ = T_cosPitch * T_cosYaw;
+	T_CosPitch2xT_CosYaw/*2CP2xCY*/ = T_cosPitch2 * T_cosYaw2;
+	T_CosPitchxT_CosYaw2/*T_CYxSZ*/ = T_cosPitch * T_cosYaw2;
+	T_cosPitch2xT_CosYaw/*T_SYxCZ*/ = T_cosPitch2 * T_cosYaw;
+
+	/*w = cx * cycz + sx * sysz;
+	x = sx * cycz - cx * sysz;
+	y = cx * sycz + sx * cysz;
+	z = cx * cysz - sx * sycz;*/
+
+	this->demensions[0] = (T_sinRoll * T_CosPitchxT_CosYaw) - (T_cosRoll * T_CosPitch2xT_CosYaw);
+	this->demensions[1] = (T_cosRoll * T_cosPitch2xT_CosYaw) + (T_sinRoll * T_CosPitchxT_CosYaw2);
+	this->demensions[2] = (T_cosRoll * T_CosPitchxT_CosYaw2) - (T_sinRoll * T_cosPitch2xT_CosYaw);
+	this->demensions[3] = (T_cosRoll * T_CosPitchxT_CosYaw) + (T_sinRoll * T_cosPitch2xT_CosYaw);
+
+}
+
+void A_Quaternion::SetFromEuler(float const _rollX, float const _pitchY, float const _yawZ) {
+
+	float T_cosRoll, T_cosPitch, T_cosYaw, T_sinRoll, T_cosPitch2, T_cosYaw2;
+
+	//I would like to formally apologize to anyone reading this code in the future for the breakdown in naming conventions here
+	float T_CosPitchxT_CosYaw, T_CosPitch2xT_CosYaw, T_CosPitchxT_CosYaw2, T_cosPitch2xT_CosYaw;
+
+	float T_roll, T_pitch, T_yaw;
+
+	T_roll = _rollX * 0.5f;
+	T_pitch = _pitchY * 0.5f;
+	T_yaw = _yawZ * 0.5f;
+
+	T_cosRoll = ::cosf(T_roll);
+	T_cosPitch = ::cosf(T_pitch);
+	T_cosYaw = ::cosf(T_yaw);
+
+	T_sinRoll = ::sinf(T_roll);
+	T_cosPitch2 = ::cosf(T_pitch);
+	T_cosYaw2 = ::cosf(T_yaw);
+
+	//these variables done this way to avoid making the same multiplication multiple times down below
+	//consider trying a different way to ease up on memory slightly while slightly slowing down arithmetic
+	//move variable declaration above
+
+	T_CosPitchxT_CosYaw/*T_CYxCZ*/ = T_cosPitch * T_cosYaw;
+	T_CosPitch2xT_CosYaw/*2CP2xCY*/ = T_cosPitch2 * T_cosYaw2;
+	T_CosPitchxT_CosYaw2/*T_CYxSZ*/ = T_cosPitch * T_cosYaw2;
+	T_cosPitch2xT_CosYaw/*T_SYxCZ*/ = T_cosPitch2 * T_cosYaw;
+
+	/*w = cx * cycz + sx * sysz;
+	x = sx * cycz - cx * sysz;
+	y = cx * sycz + sx * cysz;
+	z = cx * cysz - sx * sycz;*/
+
+	this->demensions[0] = (T_sinRoll * T_CosPitchxT_CosYaw) - (T_cosRoll * T_CosPitch2xT_CosYaw);
+	this->demensions[1] = (T_cosRoll * T_cosPitch2xT_CosYaw) + (T_sinRoll * T_CosPitchxT_CosYaw2);
+	this->demensions[2] = (T_cosRoll * T_CosPitchxT_CosYaw2) - (T_sinRoll * T_cosPitch2xT_CosYaw);
+	this->demensions[3] = (T_cosRoll * T_CosPitchxT_CosYaw) + (T_sinRoll * T_cosPitch2xT_CosYaw);
+
+}
+
+A_Quaternion::~A_Quaternion() {
+
+
+
+}
---
+++ A_Quaternion.h
@@ -0,0 +1,355 @@
+#ifndef A_QUATERNION_H
+#define A_QUATERNION_H
+
+#include "A_Vector3.h"
+
+//used for trig on getting euler vector, and square root functionality
+//#include <math.h>
+
+class A_Quaternion {
+
+public:
+
+	/**
+	//*********************************************************
+	//*********************************************************
+	//*********************************************************
+	//***********Constructors and destructor*******************
+	//*********************************************************
+	//*********************************************************
+	//*********************************************************
+	*/
+
+	/**
+	constructor which initializes all demensions of the quaternion to zero
+	*/
+	A_Quaternion();
+
+
+	/**
+	constructor which initializes all demensions of the quaternion
+	to the demensions of the vector who's pointer was passed
+	*/
+	A_Quaternion(A_Quaternion const *_AQ);
+
+
+	/**
+	constructor which initializes all demensions of the quaternion to
+	the values passed
+	*/
+	A_Quaternion(float const _x, float const _y, float const _z, float const _w);
+
+	/**
+	Constructor which initializes the X, Y, and the Z of the quaternion to the X, Y, and Z of the 3D vector
+	and the W value to _w
+	*/
+	A_Quaternion(A_Vector3 const *_AV, float const _w);
+
+	/**
+	destructor
+	*/
+	~A_Quaternion();
+
+	/**
+	//*********************************************************
+	//*********************************************************
+	//*********************************************************
+	//***************Quaternion Functions**********************
+	//*********************************************************
+	//*********************************************************
+	//*********************************************************
+	*/
+
+	//DO ME
+
+	/**
+	remove this method or fix it
+	*/
+	A_Vector3 GetEuler();
+
+	/**
+	returns the angle and the axis as a quaternion
+	*/
+	const A_Quaternion GetQuaternion(float _angle, A_Vector3 _axis);
+
+	/**
+	returns the four floats in quaternion form
+	*/
+	const A_Quaternion GetQuaternion(float _x, float _y, float _z, float _w);
+
+	/**
+	used to assist in normalizing this quaternion
+	*/
+	const float GetMagnatude();
+
+	/**
+	normalizes this quaternion making it into a unit quaternion
+	*/
+	void Normalize();
+
+	/**
+	returns the normal of this quaternion
+	*/
+	const float GetNormal();
+
+	/**
+	sets this quaternion equal to it's inverse
+	*/
+	void Conjugate();
+
+	/**
+	returns the inverse of this quaternion
+	*/
+	const A_Quaternion GetConjugate();
+
+	/**
+	//*********************************************************
+	//*********************************************************
+	//*********************************************************
+	//***************Quaternion Operators**********************
+	//*********************************************************
+	//*********************************************************
+	//*********************************************************
+	*/
+
+	/**
+	multiplies this quaternion by a float, to scale it's demensions uniformly and returns that value
+	*/
+	A_Quaternion operator * (float const _f) {
+
+		A_Quaternion T_Result;
+
+		T_Result.demensions[0] = this->demensions[0] * _f;
+		T_Result.demensions[1] = this->demensions[1] * _f;
+		T_Result.demensions[2] = this->demensions[2] * _f;
+		T_Result.demensions[3] = this->demensions[3] * _f;
+
+		return T_Result;
+
+	}
+
+	/**
+	multiplies this quaternion by a float, to scale it's demensions uniformly and assigns that value
+	to this quaternion
+	*/
+	A_Quaternion operator *= (float const _f) {
+
+		this->demensions[0] *= _f;
+		this->demensions[1] *= _f;
+		this->demensions[2] *= _f;
+		this->demensions[3] *= _f;
+
+	}
+
+	/**
+	adds to this quaternion _AQ and returns the result
+	*/
+	A_Quaternion operator + (A_Quaternion const _AQ) {
+
+		A_Quaternion T_Result;
+
+		T_Result.demensions[0] = this->demensions[0] + _AQ.demensions[0];
+		T_Result.demensions[1] = this->demensions[1] + _AQ.demensions[1];
+		T_Result.demensions[2] = this->demensions[2] + _AQ.demensions[2];
+		T_Result.demensions[3] = this->demensions[3] + _AQ.demensions[3];
+
+		return T_Result;
+
+	}
+
+	/**
+	adds to this quaternion _AQ
+	*/
+	void operator += (A_Quaternion const _AQ) {
+
+		this->demensions[0] += _AQ.demensions[0];
+		this->demensions[1] += _AQ.demensions[1];
+		this->demensions[2] += _AQ.demensions[2];
+		this->demensions[3] += _AQ.demensions[3];
+
+	}
+
+	/**
+	makes this quaternion into a copy of _AQ
+	*/
+	void operator = (A_Quaternion const _AQ) {
+
+		this->demensions[0] = _AQ.demensions[0];
+		this->demensions[1] = _AQ.demensions[1];
+		this->demensions[2] = _AQ.demensions[2];
+		this->demensions[3] = _AQ.demensions[3];
+
+	}
+
+	/**
+	returns true if all demensions of this quaternion and the demensions of _AQ are equal
+	a margin of error could be added but would be computationally expensive
+	*/
+	bool operator == (A_Quaternion const _AQ) {
+
+		if (this->demensions[0] == _AQ.demensions[0] && this->demensions[1] == _AQ.demensions[1] &&
+			this->demensions[2] == _AQ.demensions[2] && this->demensions[3] == _AQ.demensions[3])
+			return true;
+
+		else return false;
+
+	}
+
+	/**
+	returns TRUE if ANY of the demensions from _AQ ARE NOT equal to that demension within this quaternion
+	otherwise returns false
+	*/
+	bool operator != (A_Quaternion const _AQ) {
+
+		if (this->demensions[0] != _AQ.demensions[0] || this->demensions[1] != _AQ.demensions[1] ||
+			this->demensions[2] != _AQ.demensions[2] || this->demensions[3] != _AQ.demensions[3])
+			return true;
+
+		else return false;
+
+	}
+
+	/**
+	multiplies this quaternion by the quaternion _SQ and returns the result
+	*/
+	A_Quaternion operator * (A_Quaternion const *_AQ) {
+
+		A_Quaternion T_Result;
+
+		/*x = q1.x * q2.w + q1.y * q2.z - q1.z * q2.y + q1.w * q2.x;
+		y = -q1.x * q2.z + q1.y * q2.w + q1.z * q2.x + q1.w * q2.y;
+		z = q1.x * q2.y - q1.y * q2.x + q1.z * q2.w + q1.w * q2.z;
+		w = -q1.x * q2.x - q1.y * q2.y - q1.z * q2.z + q1.w * q2.w;*/
+
+		T_Result.demensions[0] = this->demensions[0] * _AQ->demensions[3] + this->demensions[1] * _AQ->demensions[2] - this->demensions[2] * _AQ->demensions[1] + this->demensions[3] * _AQ->demensions[0];
+		T_Result.demensions[1] = -this->demensions[0] * _AQ->demensions[2] + this->demensions[1] * _AQ->demensions[3] + this->demensions[2] * _AQ->demensions[0] + this->demensions[3] * _AQ->demensions[1];
+		T_Result.demensions[2] = this->demensions[0] * _AQ->demensions[1] - this->demensions[1] * _AQ->demensions[0] + this->demensions[2] * _AQ->demensions[3] + this->demensions[3] * _AQ->demensions[2];
+		T_Result.demensions[3] = -this->demensions[0] * _AQ->demensions[0] - this->demensions[1] * _AQ->demensions[1] - this->demensions[2] * _AQ->demensions[2] + this->demensions[3] * _AQ->demensions[3];
+
+		return T_Result;
+
+	}
+
+	void operator *= (A_Quaternion const *_AQ) {
+
+		this->demensions[0] = this->demensions[0] * _AQ->demensions[3] + this->demensions[1] * _AQ->demensions[2] - this->demensions[2] * _AQ->demensions[1] + this->demensions[3] * _AQ->demensions[0];
+		this->demensions[1] = -this->demensions[0] * _AQ->demensions[2] + this->demensions[1] * _AQ->demensions[3] + this->demensions[2] * _AQ->demensions[0] + this->demensions[3] * _AQ->demensions[1];
+		this->demensions[2] = this->demensions[0] * _AQ->demensions[1] - this->demensions[1] * _AQ->demensions[0] + this->demensions[2] * _AQ->demensions[3] + this->demensions[3] * _AQ->demensions[2];
+		this->demensions[3] = -this->demensions[0] * _AQ->demensions[0] - this->demensions[1] * _AQ->demensions[1] - this->demensions[2] * _AQ->demensions[2] + this->demensions[3] * _AQ->demensions[3];
+
+	}
+
+
+	/**
+	//*********************************************************
+	//*********************************************************
+	//*********************************************************
+	//***************member access*****************************
+	//*********************************************************
+	//*********************************************************
+	//*********************************************************
+	*/
+
+	/**
+	returns this quaternion's X value
+	*/
+	const float getX() {
+
+		return this->demensions[0];
+
+	}
+
+	/**
+	returns this quaternion's Y value
+	*/
+	const float getY() {
+
+		return this->demensions[1];
+
+	}
+
+	/**
+	returns this quaternion's Z value
+	*/
+	const float getZ() {
+
+		return this->demensions[2];
+
+	}
+
+	/**
+	returns this quaternion's W value
+	*/
+	const float getW() {
+
+		return this->demensions[3];
+
+	}
+
+	/**
+	access for setting X
+	*/
+	void setX(float const _x) {
+
+		this->demensions[0] = _x;
+
+	}
+
+	/**
+	access for setting Y
+	*/
+	void setY(float const _y) {
+
+		this->demensions[1] = _y;
+
+	}
+
+	/**
+	access for setting Z
+	*/
+	void setZ(float const _z) {
+
+		this->demensions[2] = _z;
+
+	}
+
+	/**
+	access for setting W
+	*/
+	void setW(float const _w) {
+
+		this->demensions[3] = _w;
+
+	}
+
+	/**
+	access for quickly setting each value
+	*/
+	void setAll(float const _x, float const _y, float const _z, float const _w) {
+
+		this->demensions[0] = _x;
+		this->demensions[1] = _y;
+		this->demensions[2] = _z;
+		this->demensions[3] = _w;
+
+	}
+
+	/**
+	allows for setting the fields of this quaternion using euler angles
+	*/
+	void SetFromEuler(A_Vector3 const &_Euler);
+
+	/**
+	allows for setting the fields of this quaternion using euler angles
+	*/
+	void SetFromEuler(float const _roll, float const _pitch, float const _yaw);
+
+
+private:
+
+	//x, y, z ,w
+	float demensions[4];
+
+};
+
+#endif
