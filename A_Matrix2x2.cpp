f7a625327623415966d65ece96e6923b5704017e
Anthony Cusimano <anthonycusimano91@hotmail.com>
2017-10-31 22:35:47 -0400

    Add files via upload
    Continued solving issues within MatrixMultiplication & corrected the method SetRowAtAddress within A_Matrix2x2
    
    TODO: continue correcting issues within MatrixMultiplication

--- A_Matrix2x2.cpp
+++ A_Matrix2x2.cpp
@@ -81,7 +81,9 @@ const A_Vector2 A_Matrix2x2::GetRowAtAddress(int const _ad) {
 
 }
 
-void A_Matrix2x2::SetRowAtAddress(int const _ad, A_Vector2* _AQ) {
+void A_Matrix2x2::SetRowAtAddress(int _ad, A_Vector2* const _AQ) {
+
+	_ad *= 2;
 
 	this->demensions[_ad] = _AQ->getX();
 	this->demensions[_ad + 1] = _AQ->getY();
